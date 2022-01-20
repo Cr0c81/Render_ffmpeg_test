c7e6dbfc230f45b5f411271de1915a4d0a5ed84a
KpoKec <sms0.mail.ru@gmail.com>
2022-01-20 11:14:33 +0300

    wip

---
+++ Assets/TextMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextMove : MonoBehaviour
+{
+    private RectTransform _rt;
+    private float         _timer;
+    public  float         _speed = 1f;
+
+    void Start()
+    {
+        _rt = (RectTransform) transform;
+    }
+
+    void Update()
+    {
+        var angle = 2f * Mathf.PI * _timer * _speed;
+        _rt.anchoredPosition =  new Vector2(10f * Mathf.Cos(angle), 10f * Mathf.Sin(angle));
+        _timer               = Mathf.Repeat(_timer +Time.deltaTime, 1f);
+        
+    }
+}
